2f463e8a2cfb0ac7479212f44d1369d448230ab2
antonprafanto <anton.prafanto@gmail.com>
2023-03-13 06:37:30 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          // brightness: Brightness.dark,
+          // scaffoldBackgroundColor: Colors.amber,
+          // primarySwatch: Colors.amber,
+          // appBarTheme: AppBarTheme(
+          //   centerTitle: true,
+          //   backgroundColor: Colors.red,
+          // ),
+          // accentColor: Colors.green,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.orange)),
+      )),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        appBar: AppBar(
+          title: Text(
+            "Theme",
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Button"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Button 2"),
+              ),
+              Text(
+                "Hello",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
